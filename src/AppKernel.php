0eab491a3496378c379467a0d75feb06af3707d6
Ben Peachey <potherca@gmail.com>
2016-10-17 20:44:56 +0000

    CHANGE:
    - Makes path parameter a Directory class
    - Loads some more bundles
    - Loads various configs from the standard edition
    - Routes can now be added from annotations in `src` & `web` directories or from  `config/routing.yml`

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -2,6 +2,7 @@
 
 namespace Potherca\Katwizy;
 
+use \Directory;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -13,6 +14,9 @@ use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
+/**
+ * @FIXME: Various hard-coded values for directories need to be read from a config file!
+ */
 class AppKernel extends Kernel
 {
     const DEBUG = 'debug';
@@ -20,11 +24,13 @@ class AppKernel extends Kernel
     const ENVIRONMENT = 'environment';
     const PRODUCTION = 'prod';
 
+    private $projectPath;
+
     use MicroKernelTrait;
 
-    final public function __construct($projectPath, array $options = [])
+    final public function __construct(Directory $projectDirectory, array $options = [])
     {
-        $this->projectPath = $projectPath;
+        $this->projectPath = $projectDirectory->path;
 
         $options = array_merge(
             [self::ENVIRONMENT => self::PRODUCTION, self::DEBUG => false],
@@ -40,19 +46,27 @@ class AppKernel extends Kernel
 
         $productionBundles = [
             \Symfony\Bundle\FrameworkBundle\FrameworkBundle::class,
+            \Symfony\Bundle\SecurityBundle\SecurityBundle::class,
             \Symfony\Bundle\TwigBundle\TwigBundle::class,
+            \Symfony\Bundle\MonologBundle\MonologBundle::class,
+            \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class,
+            \Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class,
             \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class,
+            \AppBundle\AppBundle::class,
         ];
 
         $developmentBundles = [
+            \Symfony\Bundle\DebugBundle\DebugBundle::class,
             \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class,
+            \Sensio\Bundle\DistributionBundle\SensioDistributionBundle::class,
+            // \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle::class,
         ];
 
         $loadBundles = function ($bundle) use (&$bundles) {
             $bundles[] = new $bundle;
         };
 
-        if ($this->getEnvironment() == 'dev') {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             array_walk($developmentBundles, $loadBundles);
         }
 
@@ -83,19 +97,46 @@ class AppKernel extends Kernel
 
     final public function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader)
     {
-        // PHP equivalent of `config.yml`
-        $containerBuilder->loadFromExtension('framework', [
-            'secret' => 'S0ME_SECRET',
+        $standardConfigDirectory = $this->projectPath.'/vendor/symfony/framework-standard-edition/app/config';
+        $projectConfigDirectory = $this->projectPath.'/config';
+
+        $defaultConfig = [
             'templating' => [
                 'engines' => ['twig'],
             ],
             'profiler' => [
                 "only_exceptions" =>  false,
             ],
-        ]);
+            'session' => [
+                'handler_id' => 'session.handler.native_file',
+                'save_path'  => $this->getVarDir() . '/sessions',
+            ]
+        ];
 
-        if (is_readable($this->projectPath.'/config/config.yml')) {
-            $loader->load($this->projectPath.'/config/config.yml');
+        /*/ Add require parameter if not present in project /*/
+        if (is_readable($projectConfigDirectory.'/config.yml') === false
+            && is_readable($projectConfigDirectory.'/parameters.yml') === false
+        ) {
+            $defaultConfig['secret'] = 'S0ME_SECR3T';// @FIXME: Make the secret a required environment variable!
+        }
+
+        // PHP equivalent of `config.yml`
+        $containerBuilder->loadFromExtension('framework', $defaultConfig);
+
+        /*/ Add prameters if present /*/
+        if (is_readable($projectConfigDirectory.'/parameters.yml')) {
+            $loader->load($projectConfigDirectory . '/parameters.yml');;
+        }
+
+        $loader->load($standardConfigDirectory . '/security.yml');
+
+        $loader->load($standardConfigDirectory . '/services.yml');
+
+        // (?) $loader->load($standardConfigDirectory.'/config.yml');
+
+        /*/ Add project configuration if present /*/
+        if (is_readable($projectConfigDirectory.'/config.yml')) {
+            $loader->load($projectConfigDirectory.'/config.yml');
         }
 
         /*/ configure WebProfilerBundle only if the bundle is enabled /*/
@@ -110,19 +151,29 @@ class AppKernel extends Kernel
     final public function configureRoutes(RouteCollectionBuilder $routes)
     {
         // 'kernel' is the name of a service that points to this class
-
         // optional 3rd argument is the route name
-        //$routes->add('/random/{limit}', 'kernel:randomAction');
         //$routes->add('/', 'kernel:homeAction', 'Homepage');
 
-        // import the WebProfilerRoutes, only if the bundle is enabled
+        // import the WebProfilerRoutes if the bundle is enabled
         if (isset($this->bundles['WebProfilerBundle'])) {
             $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml', '/_wdt');
             $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml', '/_profiler');
         }
 
-        /*/ load the annotation routes /*/
-        $routes->import($this->projectPath.'/src/Controller/', '/', 'annotation');
+        /*/ load routes from source annotations /*/
+        if (is_dir($this->projectPath.'/src/')) {
+            $routes->import($this->projectPath.'/src/', '/', 'annotation');
+        }
+
+        /*/ load routes from web annotations /*/
+        if (is_dir($this->projectPath.'/web/')) {
+            $routes->import($this->projectPath.'/web/', '/', 'annotation');
+        }
+
+        /*/ load routes from configurayion file /*/
+        if (is_readable($this->projectPath.'/config/routing.yml')) {
+            $routes->import($this->projectPath.'/config/routing.yml');
+        }
     }
 }
 
