b0cd96382d5a061a992cf88b062ce3ceec3fa018
Ben Peachey <potherca@gmail.com>
2016-10-17 20:39:35 +0000

    CHANGE: Adds the ability to set loader to bootstrap.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,24 +5,28 @@ namespace Potherca\Katwizy;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-/*/ Autoloader /*/
-$scriptFileName = $_SERVER['SCRIPT_FILENAME'];
-$webDirectory = dirname($scriptFileName);
-$rootDirectory = dirname($webDirectory);
-$loader = require $rootDirectory.'/vendor/autoload.php';
+if ($loader === null) {
+    /*/ Autoloader /*/
+    $scriptFileName = $_SERVER['SCRIPT_FILENAME'];
+    $webDirectory = dirname($scriptFileName);
+    $rootDirectory = dirname($webDirectory);
+
+    $loader = require $rootDirectory.'/vendor/autoload.php';
+}
+
+$reflector = new \ReflectionObject($loader);
+// Autoloader at /vendor/composer/ClassLoader.php
+$rootDirectory = dirname(dirname(dirname($reflector->getFileName())));
 
 /*/ Autoload annotations /*/
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 /*/ Run Framework /*/
 $request = Request::createFromGlobals();
-$kernel = new AppKernel(
-    $rootDirectory,
-    [/* @TODO: Grab config from the environment */
-        AppKernel::ENVIRONMENT => AppKernel::DEVELOPMENT,
-        AppKernel::DEBUG => true
-    ]
-);
+$kernel = new AppKernel($rootDirectory, [
+    AppKernel::ENVIRONMENT => AppKernel::DEVELOPMENT,// @TODO: Grab config from the environment
+    AppKernel::DEBUG => true // @TODO: Match debugtoken from cookie/header/url to config:debugtoken
+]);
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
