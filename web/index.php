b7d33efaf888d75ab809485b3203a3f1d5e6e5a4
Ben Peachey <potherca@gmail.com>
2016-11-07 20:45:35 +0000

    CHANGE: Makes the index file look for autoloader.

--- web/index.php
+++ web/index.php
@@ -2,6 +2,22 @@
 
 namespace Potherca\Katwizy;
 
-require dirname(__DIR__) . '/src/bootstrap.php';
+$autoloadFiles = [
+    dirname(__DIR__) . '/vendor/autoload.php',
+    dirname(dirname(dirname(__DIR__))) . '/autoload.php'
+];
+
+if (file_exists($autoloadFiles[0])) {
+    $loader = require $autoloadFiles[0];
+} elseif (file_exists($autoloadFiles[1])) {
+    $loader = require $autoloadFiles[1];
+} else {
+    throw new \RuntimeException('Could not find "vendor/autoload.php". Has `composer install` been run?');
+}
+
+Potherca\Katwizy\Bootstrap::run(
+    $loader,
+    Symfony\Component\HttpFoundation\Request::createFromGlobals()
+);
 
 /*EOF*/
